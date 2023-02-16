7c31b7b25e21b2b618bff00968a5fcee7c085be2
Aleksandr Parmuzin <alexandr.parmuzin1@gamil.com>
2023-02-17 00:40:55 +0300

    Debugger incident for 2h X?D

--- src/controllers/beverages.controller.ts
+++ src/controllers/beverages.controller.ts
@@ -1,30 +1,41 @@
-import { Prisma, PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const getBeverages = async (req: Request, res: Response) => {
   try {
+    const beverages = await prisma.beverages.findMany();
     
+    return res.status(200).json({ data: beverages });
   } catch (error) {
-
+    const errorMessage = (error as Error).message;
+    return res.status(400).json({ msg: errorMessage });
   }
-}
+};
 
 const getOneBeverage = async (req: Request, res: Response) => {
+  const { beverageUID } = req.params;
   try {
+    const beverage = await prisma.beverages.findUnique({
+      where: { UID: beverageUID },
+    });
 
+    return res.status(200).json({ data: beverage });
   } catch (error) {
-    
+    const errorMessage = (error as Error).message;
+    return res.status(400).json({ msg: errorMessage });
   }
-}
+};
 
-const createBeverage = async (req: Request, res: Response) => {
+const createBeverage = async (req: Request, res: Response) => { 
   const { name, type, price, description, volume, alcPercentage } = req.body;
   try {
+    const rap = 'trap';
+    
     const result = await prisma.beverages.create({
       data: {
-        name,
+        name, 
         type,
         price,
         description,
@@ -32,8 +43,11 @@ const createBeverage = async (req: Request, res: Response) => {
         alcPercentage,
       },
     });
-    res.json(result);
-  } catch (error) {}
+    return res.status(200).json({ msg: `Beverage was created:` });
+  } catch (error) {
+    const errorMessage = (error as Error).message;
+    return res.status(400).json({ msg: errorMessage });
+  }
 };
 
 const beveragesController = {
